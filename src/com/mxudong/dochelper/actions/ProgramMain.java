adead090f4064278daf0d4d17d3cd9df50fcc841
MXuDong <2941884109@qq.com>
2019-07-15 14:07:39 +0800

    更新结构

--- src/com/mxudong/dochelper/actions/ProgramMain.java
+++ src/com/mxudong/dochelper/actions/ProgramMain.java
@@ -3,18 +3,10 @@ package com.mxudong.dochelper.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.javadoc.PsiDocComment;
-import com.intellij.psi.javadoc.PsiDocTag;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.mxudong.dochelper.common.DescUtil;
+import com.mxudong.dochelper.common.DescriptionItem;
 import com.mxudong.dochelper.common.GetSetterManager;
 import com.mxudong.dochelper.view.ShowDocHelperView;
 import org.jetbrains.annotations.NotNull;
@@ -34,7 +26,6 @@ public class ProgramMain extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         // TODO: insert action logic here
 
-        Project project = e.getData(PlatformDataKeys.PROJECT);
         PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
         Editor editor = e.getData(CommonDataKeys.EDITOR);
 
@@ -42,16 +33,12 @@ public class ProgramMain extends AnAction {
             return;
         }
 
-        PsiElement element = psiFile.findElementAt(editor.getCaretModel().getOffset());
         Document document = editor.getDocument();
-        String[] doc = DescUtil.GetDoc(document.getText());
         ShowDocHelperView showDocHelperView = GetSetterManager.getShowDocHelperView();
+        DescriptionItem descriptionItem = new DescriptionItem(document.getText());
         if(showDocHelperView != null){
             showDocHelperView.changeText("");
-            for(String str : doc){
-                System.out.println(str);
-                showDocHelperView.changeText(showDocHelperView.getText() + str);
-            }
+            showDocHelperView.changeText(descriptionItem.getDescInfo());
         }
     }
 }
---
+++ src/com/mxudong/dochelper/common/DescriptionItem.java
@@ -0,0 +1,33 @@
+package com.mxudong.dochelper.common;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Dong
+ * @since 1.0
+ */
+
+public class DescriptionItem {
+
+    private String document;
+
+    private final static Pattern DESC_PATTERN = Pattern.compile("\\n\\s*\\*\\s*([^\\n]*)");
+
+    public DescriptionItem(String document) {
+        this.document = document;
+    }
+
+    public String getDescInfo(){
+        Matcher matcher = DESC_PATTERN.matcher(document);
+        StringBuilder result = new StringBuilder();
+        while (matcher.find()){
+            int length = matcher.groupCount();
+            for(int i = 0; i < length; i++){
+                result.append(matcher.group(i +1));
+            }
+        }
+
+        return result.toString();
+    }
+}
